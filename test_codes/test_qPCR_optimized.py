af5d2dcbf9e1acc9d0edca7ba7c01e4d709610d9
Ashir Borah <ashiraseesh@gmail.com>
2023-04-10 14:38:03 -0700

    Fixed bug to correctly distribute for qPCR

--- test_codes/test_qPCR_optimized.py
+++ test_codes/test_qPCR_optimized.py
@@ -21,11 +21,12 @@ metadata = {
     'protocolName': 'Transfer Components from Separate Thermoblock Tubes to 384-Well Plate with Pandas'
 }
 
-def map_tube_to_well(dataframe, p384, temp_labware, primer_tube_positions, sample_tube_positions, p20_single):
+def map_tube_to_well(dataframe, p384, temp_labware, primer_tube_positions, sample_tube_positions, p20_single, p300_single, protocol):
     for index, col in dataframe.items():
         ## Check if the primer is the same, then use the distribute function
         primers_in_columns = set([x.split(',')[0] for x in col.values])
         if len(primers_in_columns) != 1:
+            protocol.comment('Multiple primers in the column')
             ## If there are multiple primers in the column, then we need to distribute the primer to each well separately
             for row, location in enumerate(col):
                 [primer, sample] = [x.strip()  for x in location.split(',')]
@@ -40,11 +41,19 @@ def map_tube_to_well(dataframe, p384, temp_labware, primer_tube_positions, sampl
                 p20_single.transfer(2.5, primer_source_well, dest_well)
                 p20_single.transfer(2.5, sample_source_well, dest_well)
         else:
+            protocol.comment('Only one primer in the column')
+            ## If there is only one primer in the column, then we can distribute the primer to all wells in the column
+            primer = primers_in_columns.pop()
+            primer_source_well = primer_tube_positions.loc[primer, 'Well']
+            primer_source_well = temp_labware.wells_by_name()[primer_source_well]
+
+            p300_single.distribute(2.5, primer_source_well, [p384.wells()[index * len(dataframe.index) + row] for row, location in enumerate(col)])
+            
             for row, location in enumerate(col):
                 [primer, sample] = [x.strip()  for x in location.split(',')]
                 primer_source_well = primer_tube_positions.loc[primer, 'Well']
                 primer_source_well = temp_labware.wells_by_name()[primer_source_well]
-                p20_single.distribute(2.5, primer_source_well, [p384.wells()[index * len(dataframe.index) + row] for row, location in enumerate(col)])
+                
                 #print('primer {} sample {}'.format(primer, sample))
                 sample_source_well = sample_tube_positions.loc[sample, 'Well']
                 dest_well = p384.wells()[index * len(dataframe.index) + row]
@@ -64,6 +73,7 @@ def run(protocol: protocol_api.ProtocolContext):
     tiprack_20ul_3 = protocol.load_labware('opentrons_96_filtertiprack_20ul', '9')
     tiprack_20ul_4 = protocol.load_labware('opentrons_96_filtertiprack_20ul', '10')
     tiprack_20ul_5 = protocol.load_labware('opentrons_96_filtertiprack_20ul', '11')
+    tiprack_200ul_1 = protocol.load_labware('opentrons_96_filtertiprack_200ul', '5')
 
   
     temp_labware = temp_mod.load_labware(
@@ -78,7 +88,8 @@ def run(protocol: protocol_api.ProtocolContext):
     protocol.pause('Thermoblock up to specific temp of 4C')
 
     # Pipettes
-    p20_single = protocol.load_instrument('p20_single_gen2', 'right', tip_racks=[tiprack_20ul_1, tiprack_20ul_2, tiprack_20ul_3, tiprack_20ul_4, tiprack_20ul_5])
+    p20_single = protocol.load_instrument('p20_single_gen2', 'right', tip_racks=[tiprack_20ul_1, tiprack_20ul_2, tiprack_20ul_3])
+    p300_single = protocol.load_instrument('p300_single_gen2', 'left', tip_racks=[tiprack_200ul_1])
 
     # Read CSV files
     transfer_data = pd.read_csv('transfer_locations.csv',header=None)  # Replace with the actual file name
@@ -93,4 +104,4 @@ def run(protocol: protocol_api.ProtocolContext):
     #import pdb; pdb.set_trace()
 
     # Transfer components
-    map_tube_to_well(transfer_data, p384, temp_labware, primer_tube_positions, sample_tube_positions, p20_single)
+    map_tube_to_well(transfer_data, p384, temp_labware, primer_tube_positions, sample_tube_positions, p20_single, p300_single, protocol)
