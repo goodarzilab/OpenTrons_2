611b91edec8a9bf096fc7ce0ed0a505d32645da9
Ashir Borah <ashiraseesh@gmail.com>
2023-04-21 15:17:24 -0700

    Changed pipetttes to p20

--- qPCR/qPCR_optimized_column_primer.py
+++ qPCR/qPCR_optimized_column_primer.py
@@ -20,7 +20,7 @@ metadata = {
     'protocolName': 'Transfer Components from Separate Thermoblock Tubes to 384-Well Plate with Pandas'
 }
 
-def map_tube_to_well(dataframe, p384, temp_labware, primer_tube_positions, sample_tube_positions, p20_single, p300_single, protocol):
+def map_tube_to_well(dataframe, plate_384, temp_labware, primer_tube_positions, sample_tube_positions, p20_single, protocol):
     for index, col in dataframe.items():
         ## Check if the primer is the same, then use the distribute function
         primers_in_columns = set([x.split(',')[0] for x in col.values])
@@ -32,7 +32,7 @@ def map_tube_to_well(dataframe, p384, temp_labware, primer_tube_positions, sampl
                 #print('primer {} sample {}'.format(primer, sample))
                 primer_source_well = primer_tube_positions.loc[primer, 'Well']
                 sample_source_well = sample_tube_positions.loc[sample, 'Well']
-                dest_well = p384.wells()[index * len(dataframe.rows) + row]
+                dest_well = plate_384.wells()[index * len(dataframe.rows) + row]
                 primer_source_well = temp_labware.wells_by_name()[primer_source_well]
                 sample_source_well = temp_labware.wells_by_name()[sample_source_well]
 
@@ -46,7 +46,7 @@ def map_tube_to_well(dataframe, p384, temp_labware, primer_tube_positions, sampl
             primer_source_well = primer_tube_positions.loc[primer, 'Well']
             primer_source_well = temp_labware.wells_by_name()[primer_source_well]
 
-            p300_single.distribute(2.5, primer_source_well, [p384.wells()[index * len(dataframe.index) + row] for row, location in enumerate(col)])
+            p20_single.distribute(2.5, primer_source_well, [plate_384.wells()[index * len(dataframe.index) + row] for row, location in enumerate(col)])
             
             for row, location in enumerate(col):
                 [primer, sample] = [x.strip()  for x in location.split(',')]
@@ -55,7 +55,7 @@ def map_tube_to_well(dataframe, p384, temp_labware, primer_tube_positions, sampl
                 
                 #print('primer {} sample {}'.format(primer, sample))
                 sample_source_well = sample_tube_positions.loc[sample, 'Well']
-                dest_well = p384.wells()[index * len(dataframe.index) + row]
+                dest_well = plate_384.wells()[index * len(dataframe.index) + row]
                 sample_source_well = temp_labware.wells_by_name()[sample_source_well]
 
                 # Transfer primer and sample to the destination well
@@ -63,14 +63,19 @@ def map_tube_to_well(dataframe, p384, temp_labware, primer_tube_positions, sampl
 
 def run(protocol: protocol_api.ProtocolContext):
     # Labware
-    primer_tubes = protocol.load_labware('opentrons_24_tuberack_generic_2ml_screwcap', '1')
-    sample_tubes = protocol.load_labware('opentrons_24_tuberack_generic_2ml_screwcap', '2')
-    p384 = protocol.load_labware('corning_384_wellplate_112ul_flat', '3')
-    temp_mod = protocol.load_module('temperature module gen2', '4')
+    temp_mod = protocol.load_module('temperature module gen2', '1')
+    thermoblock = temp_mod.load_labware(
+        "opentrons_24_aluminumblock_nest_1.5ml_snapcap",
+        label="Temperature-Controlled Stocks",
+    # )
+    # primer_tubes = protocol.load_labware('opentrons_24_tuberack_generic_2ml_screwcap', '1')
+    # sample_tubes = protocol.load_labware('opentrons_24_tuberack_generic_2ml_screwcap', '2')
+    plate_384 = protocol.load_labware('corning_384_wellplate_112ul_flat', '3')
+    temp_mod = protocol.load_module('temperature module gen2', '1')
     tiprack_20ul_1 = protocol.load_labware('opentrons_96_filtertiprack_20ul', '7')
     tiprack_20ul_2 = protocol.load_labware('opentrons_96_filtertiprack_20ul', '8')
     tiprack_20ul_3 = protocol.load_labware('opentrons_96_filtertiprack_20ul', '9')
-    tiprack_200ul_1 = protocol.load_labware('opentrons_96_filtertiprack_200ul', '5')
+    #tiprack_200ul_1 = protocol.load_labware('opentrons_96_filtertiprack_200ul', '5')
 
   
     temp_labware = temp_mod.load_labware(
@@ -79,14 +84,13 @@ def run(protocol: protocol_api.ProtocolContext):
     )
     
     # Set temp to 4C
-    temp_mod.set_temperature(celsius=4)
+    ## Change this to 4C during the actual run
+    temp_mod.set_temperature(celsius=22)
     temp_mod.status  # 'holding at target'
-    
-    protocol.pause('Thermoblock up to specific temp of 4C')
 
     # Pipettes
     p20_single = protocol.load_instrument('p20_single_gen2', 'right', tip_racks=[tiprack_20ul_1, tiprack_20ul_2, tiprack_20ul_3])
-    p300_single = protocol.load_instrument('p300_single_gen2', 'left', tip_racks=[tiprack_200ul_1])
+    #p300_single = protocol.load_instrument('p300_single_gen2', 'left', tip_racks=[tiprack_200ul_1])
 
     # Read CSV files
     transfer_data = pd.read_csv('transfer_locations.csv',header=None)  # Replace with the actual file name
@@ -101,4 +105,4 @@ def run(protocol: protocol_api.ProtocolContext):
     #import pdb; pdb.set_trace()
 
     # Transfer components
-    map_tube_to_well(transfer_data, p384, temp_labware, primer_tube_positions, sample_tube_positions, p20_single, p300_single, protocol)
+    map_tube_to_well(transfer_data, plate_384, temp_labware, primer_tube_positions, sample_tube_positions, p20_single, protocol)
